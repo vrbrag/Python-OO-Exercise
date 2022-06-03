79e6f475381df47963412dbc84d43e8153a2ab61
Vanessa Bragdon <vanessabragdon@Vanessas-MacBook-Pro.local>
2022-06-02 23:38:41 -1000

    parse file into a readable list

--- wordfinder.py
+++ wordfinder.py
@@ -2,27 +2,29 @@ import random
 
 """Word Finder: finds random words from a dictionary."""
 
-with open('simple.txt') as file:
-    file_contents = file.read()
-    print(file_contents) 
-    print(type(list(file_contents))) 
+class WordFinder:
+    ...
 
+    def __init__(self, path):
+        """Read dictionary """
 
-
-# class WordFinder:
-#     ...
-
-#     # wf = open("words.txt", "r")
-      
-#     # for word in wf:
-#     #     return random.choices(word)
-
-#     def __init__(self, path):
-#         file = open(path)
-#         self.words = file.readlines()
-
-#         print(f"{len(self.words)} words read")
+        file = open(path)
+        self.words = self.parse(file)
+        print(f"{len(self.words)} words read")
     
-#     def random(self):
-#         """Return random word(s)"""
-#         return random.choice(self.words)
+
+    def parse(self, file):
+        """Parse file into list"""
+
+        list = []
+        for word in file:
+            w = word.strip()
+            list.append(w)
+        return list
+        # return [word.strip() for word in file]
+
+
+    def random(self):
+        """Return random word(s)"""
+
+        return random.choice(self.words)
